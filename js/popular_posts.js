2b0a798c0145891d2f84eeea2299589887314232
eidomar72 <eidomar72@gmail.com>
2023-06-03 10:32:07 -0400

    4

--- js/popular_posts.js
+++ js/popular_posts.js
@@ -28,7 +28,60 @@ function queryReports() {
   }).then(displayResults, console.error.bind(console));
 }
 
-function displayResults(response) {
-  var formattedJson = JSON.stringify(response.result, null, 2);
-  document.getElementById('query-output').value = formattedJson;
-}
+// Fetch popularity data from Google Analytics
+     function fetchPopularityData() {
+       gapi.client.analyticsreporting.reports
+         .batchGet({
+           requestBody: {
+             reportRequests: [
+               {
+                 viewId: 'YOUR_VIEW_ID',
+                 dateRanges: [
+                   {
+                     startDate: 'YOUR_START_DATE', // e.g., '2023-01-01'
+                     endDate: 'YOUR_END_DATE', // e.g., '2023-05-31'
+                   },
+                 ],
+                 metrics: [
+                   { expression: 'ga:pageviews' }, // Adjust the metric based on your needs
+                 ],
+                 dimensions: [
+                   { name: 'ga:pagePath' }, // Adjust the dimension based on your needs
+                 ],
+                 orderBys: [
+                   { fieldName: 'ga:pageviews', sortOrder: 'DESCENDING' },
+                 ],
+                 pageSize: 3, // Number of top popular posts to fetch
+               },
+             ],
+           },
+         })
+         .then((response) => {
+           const popularityData = response.result.reports[0].data.rows;
+
+           // Combine popularity data with blog data
+           const blogsWithPopularity = blogs.map((blog) => {
+             const matchingRow = popularityData.find((row) => row.dimensions[0] === blog.url);
+             const popularity = matchingRow ? +matchingRow.metrics[0].values[0] : 0;
+
+             return { ...blog, popularity };
+           });
+
+           // Sort the blogs by popularity (descending order)
+           const sortedBlogs = blogsWithPopularity.sort((a, b) => b.popularity - a.popularity);
+
+           // Display the top popular posts
+           const popularPostsElement = document.getElementById('popular-posts');
+           sortedBlogs.slice(0, 3).forEach((blog) => {
+             const listItem = document.createElement('li');
+             const link = document.createElement('a');
+             link.href = blog.url;
+             link.textContent = blog.title;
+             listItem.appendChild(link);
+             popularPostsElement.appendChild(listItem);
+           });
+         })
+         .catch((error) => {
+           console.error('Error fetching popularity data:', error);
+         });
+     }
