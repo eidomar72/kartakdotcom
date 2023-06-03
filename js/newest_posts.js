c05d85039e94f4a0b9d3d0c247d8395d3d0d0f11
eidomar72 <eidomar72@gmail.com>
2023-06-03 07:15:22 -0400

    c

--- js/newest_posts.js
+++ js/newest_posts.js
@@ -1,47 +1,55 @@
 // JavaScript code for parsing and displaying the latest blogs
-function parseAndDisplayBlogs() {
-  fetch('https://kartak.co/blog.html')
-  .then(response => response.text())
-  .then(data => {
-    // Create a temporary element to hold the HTML content
-    const tempElement = document.createElement('div');
-    tempElement.innerHTML = data;
+  function parseAndDisplayBlogs() {
+    fetch('https://kartak.co/blog.html')
+      .then(response => response.text())
+      .then(data => {
+        // Create a temporary element to hold the HTML content
+        const tempElement = document.createElement('div');
+        tempElement.innerHTML = data;
 
-    // Extract the blog information using DOM manipulation
-    const blogElements = tempElement.querySelectorAll('.blog');
-    const blogs = [];
+        // Extract the blog information using DOM manipulation
+        const blogElements = tempElement.querySelectorAll('.blog');
+        const blogs = [];
 
-    blogElements.forEach(blogElements => {
-      const blog = {
-        title: blogElements.querySelector('.blog-title').innerText,
-        content: blogElements.querySelector('.blog-content').innerText
-      };
-      blogs.push(blog);
-    });
+        blogElements.forEach(blogElement => {
+          const titleElement = blogElement.querySelector('.blog-title');
+          const contentElement = blogElement.querySelector('.blog-content');
 
-    // Display the latest 3 blogs
-    const blogContainer = document.getElementById('blog-container');
+          // Check if both titleElement and contentElement exist
+          if (titleElement && contentElement) {
+            const blog = {
+              title: titleElement.innerText,
+              content: contentElement.innerText
+            };
+            blogs.push(blog);
+          }
+        });
 
-    for (let i = 0; i < 3 && i < blogs.length; i++) {
-      const blog = blogs[i];
+        // Display the latest 3 blogs
+        const blogContainer = document.getElementById('blog-container');
 
-      // Create HTML elements for the blog title and content
-      const blogElements = document.createElement('div');
-      const titleElement = document.createElement('h2');
-      const contentElement = document.createElement('p');
+        for (let i = 0; i < 3 && i < blogs.length; i++) {
+          const blog = blogs[i];
 
-      // Set the text content of the blog title and content
-      titleElement.textContent = blog.title;
-      contentElement.textContent = blog.content;
+          // Create HTML elements for the blog title and content
+          const blogElement = document.createElement('div');
+          const titleElement = document.createElement('h2');
+          const contentElement = document.createElement('p');
 
+          // Set the text content of the blog title and content
+          titleElement.textContent = blog.title;
+          contentElement.textContent = blog.content;
 
-      // Append the blog title and content elements to the main container
-      blogElements.appendChild(titleElement);
-      blogElements.appendChild(contentElement);
-      blogContainer.appendChild(blogElements);
+          // Append the blog title and content elements to the main container
+          blogElement.appendChild(titleElement);
+          blogElement.appendChild(contentElement);
+          blogContainer.appendChild(blogElement);
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 
-    }
-  })
-}
-// Call the function to parse and display the latest blogs
-parseAndDisplayBlogs();
+  // Call the function to parse and display the latest blogs
+  parseAndDisplayBlogs();
