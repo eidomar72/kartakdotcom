7f5b2418ac673fa3fc9621c49d2e4c56080cf7c8
eidomar72 <eidomar72@gmail.com>
2023-06-03 06:32:10 -0400

    d
    s

--- js/newest_posts.js
+++ js/newest_posts.js
@@ -1,41 +1,43 @@
-// Function to parse and display the latest blogs
-function parseAndDisplayBlogs() {
-  // Fetch the HTML content of the website page
-  fetch('URL_OF_WEBSITE_PAGE')
-    .then(response => response.text())
-    .then(data => {
-      // Create a temporary element to hold the HTML content
-      const tempElement = document.createElement('div');
-      tempElement.innerHTML = data;
+.then(response => response.text())
+.then(data => {
+    // Create a temporary element to hold the HTML content
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = data;
 
-      // Extract the blog information using DOM manipulation
-      const blogElements = tempElement.querySelectorAll('.blog'); // Replace '.blog' with the appropriate CSS selector for your blog elements
-      const blogs = [];
+    // Extract the blog information using DOM manipulation
+    const blogElements = tempElement.querySelectorAll('.blog');  // Replace '.blog' with the appropriate CSS selector for your blog elements
+    const blogs = [];
 
-      blogElements.forEach(blogElement => {
+    blogElements.forEach(blogElement => {
         const blog = {
-          title: blogElement.querySelector('.blog-title').innerText, // Replace '.blog-title' with the appropriate CSS selector for the blog title
-          content: blogElement.querySelector('.blog-content').innerText // Replace '.blog-content' with the appropriate CSS selector for the blog content
+            title: blogElement.querySelector('.blog-title').innerText, // Replace '.blog-title' with the appropriate CSS selector for the blog title
+            //content: blogElement.querySelector('.blog-content').innerText // Replace '.blog-content' with the appropriate CSS selector for the blog content
         };
         blogs.push(blog);
-      });
-
-      // Display the blogs on the desired page
-      const blogContainer = document.getElementById('blog-container'); // Replace 'blog-container' with the ID or selector of the container where you want to display the blogs
-
-      blogs.forEach(blog => {
-        const blogElement = document.createElement('div');
-        blogElement.innerHTML = `
-          <h2>${blog.title}</h2>
-          <p>${blog.content}</p>
-        `;
-        blogContainer.appendChild(blogElement);
-      });
-    })
-    .catch(error => {
-      console.error('Error:', error);
     });
-}
 
+    // Display the latest 3 blogs
+    const blogContainer = document.getElementById('blog-container');
+
+    // Loop through the latest 3 blogs
+    for (let i = 0; i < 3 && i < blogs.length; i++) {
+        const blog = blogs[i];
+
+        // Create HTML elements for the blog title and content
+        const blogElement = document.createElement('div');
+        const titleElement = document.createElement('h2');
+        //const contentElement = document.createElement('p');
+
+        // Set the text content of the blog title and content
+        titleElement.textContent = blog.title;
+        //contentElement.textContent = blog.content;
+
+        // Append the blog title and content elements to the main container
+        blogElement.appendChild(titleElement);
+        //blogElement.appendChild(contentElement);
+        blogContainer.appendChild(blogElement);
+    }
+})
+}s
 // Call the function to parse and display the latest blogs
 parseAndDisplayBlogs();
