dab7560c4b79401da4d63a13d53962fecfa98010
eidomar72 <eidomar72@gmail.com>
2023-06-04 13:01:39 -0400

    k

--- js/newest_posts.js
+++ js/newest_posts.js
@@ -15,13 +15,15 @@ function parseAndDisplayBlogs() {
         const linkElement = blogElement.querySelector('a');
         const titleElement = blogElement.querySelector('.blog-title');
         const contentElement = blogElement.querySelector('.blog-content');
+        const dateElement = blogElement.querySelector('.blog-date');
 
         // Check if all required elements exist
-        if (linkElement && titleElement && contentElement) {
+        if (linkElement && titleElement && contentElement && dateElement) {
           const blog = {
             url: linkElement.href,
             title: titleElement.textContent,
-            content: contentElement.textContent
+            content: contentElement.textContent,
+            date: dateElement.textContent,
           };
           blogs.push(blog);
         }
@@ -33,30 +35,34 @@ function parseAndDisplayBlogs() {
       for (let i = 0; i < 3 && i < blogs.length; i++) {
         const blog = blogs[i];
 
-        // Create HTML elements for the blog title and content
+        // Create HTML elements for the blog title, content, and date
         const blogElement = document.createElement('div');
         const titleElement = document.createElement('h2');
         const contentElement = document.createElement('p');
+        const dateElement = document.createElement('span');
 
         // Create an anchor element for the title with the appropriate href
         const titleLink = document.createElement('a');
         titleLink.href = blog.url;
         titleLink.textContent = blog.title;
-
+        dateElement.textContent = blog.date;
 
         // Set the text content of the content element
         contentElement.textContent = blog.content;
 
-        // Append the title link and content elements to the blog element
+        // Append the title link, content, and date elements to the blog element
         blogElement.appendChild(titleLink);
         blogElement.appendChild(contentElement);
+        blogElement.appendChild(dateElement);
 
         // Append the blog element to the main container
         blogContainer.appendChild(blogElement);
 
         if (i < 2) {
           // Add spacing between each title (adjust the value according to your preference)
-          blogContainer.appendChild(document.createElement('hr'));
+          const spacingElement = document.createElement('div');
+          spacingElement.classList.add('spacing');
+          blogContainer.appendChild(spacingElement);
         }
       }
     })
